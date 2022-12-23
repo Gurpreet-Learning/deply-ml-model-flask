6bc98e9abba36a818d62a8d7bfe563b4deefadcb
datascientist5 <deol.developer@gmail.com>
2022-12-22 17:16:05 -0800

    Home method added in app.py and test_app.py

--- test_app.py
+++ test_app.py
@@ -1,5 +1,9 @@
-from app import hello_world
+from app import hello_world, home
 
 
 def test_hello_world():
     assert hello_world() == "Hello World!"
+
+
+def test_home():
+    assert home() == "Home"
